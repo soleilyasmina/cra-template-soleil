7d038745e5ca93ea52480a6f53d2749d6f885de9
soleilyasmina <mounlasolomon@gmail.com>
2021-10-06 14:31:20 -0700

    add switch and route to App.js for immediate routing

--- template/src/App.js
+++ template/src/App.js
@@ -1,9 +1,16 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      { /* get hacking! */ }
+      <Switch>
+        <main>
+          <Route exact path="/">
+            Welcome to the root route!
+          </Route>
+        </main>
+      </Switch>
     </div>
   );
 }
